6859107f37c185414c483e11329a37a026092f83
Deives Collins <deivescollins@gmail.com>
2017-02-02 23:15:17 -0500

    Update mrbox.py

--- src/mrbox.py
+++ src/mrbox.py
@@ -3,7 +3,7 @@ Mr. Box:
 Practice box-sorting problem solution script in Python 3.5.2
 
 The MIT License:
-Copyright (c) 2017 Deives Collins
+Copyright (c) 2017 Deives Collins ("Rammschnev")
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
